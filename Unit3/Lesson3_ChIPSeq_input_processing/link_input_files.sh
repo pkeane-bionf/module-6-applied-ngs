61bcbb668c52f6d2d6306c81fe0d3c8c62ba6585
Peter Keane <p.keane@bham.ac.uk>
2024-08-12 15:35:41 +0100

    Added Unit3

---
+++ Unit3/Lesson3_ChIPSeq_input_processing/link_input_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#create dynamic links, this saves storage space
+ln -s /rds/projects/c/cazierj-msc-bioinfo-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_input_processing/HB_Lmo2_aligned_sorted.bam .
+ln -s /rds/projects/c/cazierj-msc-bioinfo-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_input_processing/HB_Tal1_aligned_sorted.bam .
+ln -s /rds/projects/c/cazierj-msc-bioinfo-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_input_processing/HB_input_aligned_sorted.bam .
---
+++ Unit3/Lesson3_ChIPSeq_peak_calling/link_input_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#create dynamic links, this saves storage space
+ln -s /rds/projects/c/cazierj-msc-bioinf-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_peak_calling/HB_Lmo2_aligned_sorted_noDups.bam .
+ln -s /rds/projects/c/cazierj-msc-bioinf-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_peak_calling/HB_Tal1_aligned_sorted_noDups.bam .
+ln -s /rds/projects/c/cazierj-msc-bioinf-dl/data/module-6-applied-ngs/Unit3/Lesson3_ChIPSeq_peak_calling/HB_input_aligned_sorted_noDups.bam .
